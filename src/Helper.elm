8d94ba741c1f75628adb943650f88806a5a1cb05
Jose <josealbertop11molino@gmail.com>
2026-02-15 18:39:27 -0600

    idk

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,117 @@
 module Helper exposing (..)
+import Html
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+add3 : Float -> Float -> Float -> Float
+add3 int1 int2 int3 =
+    int1 + int2 + int3
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 fun =
+    fun int1 int2
+
+langs : List { name : String, releaseYear : Int, currentVersion : String }
+langs =
+    [ { name = "elm"
+      , releaseYear = 2012
+      , currentVersion = "0.19.1"
+      }
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAscipt 2056"
+      }
+    ]
+
+--Ejercicio 1
+
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+languageNames list =
+    List.map .name list
+
+--Ejercicio 2
+
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto"
+      , uType = "Student"
+      }
+    , { name = "Mitsiu"
+      , uType = "Professor"
+      }
+    ]
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    List.map
+        (\user ->
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+        )
+        list
+
+--Ejercicio 3
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+videogames : List Videogame
+videogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 200
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of Time"
+      , releaseYear = 1998
+      , available = True
+      , downloads = 200
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres list =
+    List.map .genres list
+
+--Ejercicio 4
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16", model = "sabe", brand = "Hp", screenSize = "idk" }
+
+main : Html.Html msg
+main =
+    Html.div []
+        [ Html.h1 [] [ Html.text "My laptop" ]
+        , Html.div []
+            [ Html.ul []
+                [ Html.li []
+                    [ Html.text ("Ram: " ++ myLaptop.ram) ]
+                , Html.li []
+                    [ Html.text ("Modelo: " ++ myLaptop.model) ]
+                , Html.li []
+                    [ Html.text ("Marca: " ++ myLaptop.brand) ]
+                , Html.li []
+                    [ Html.text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
